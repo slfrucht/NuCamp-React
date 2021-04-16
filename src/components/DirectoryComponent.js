e066c8109f68f6c34e02af1327969265b9349a6f
slfrucht <sfrucht@lmi.net>
2021-04-16 13:57:05 -0700

    added data files for campsites, comments, etc.

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -2,9 +2,9 @@ import React from "react";
 import {Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
 import { PopperPlacements } from "reactstrap/lib/utils";
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}) {
 return(
-    <Card onClick = {() => onClick(campsite.id)}>
+    <Card >
     <CardImg src={campsite.image} alt={campsite.name}></CardImg>
     <CardImgOverlay>
         <CardTitle>{campsite.name}</CardTitle>
@@ -17,7 +17,7 @@ function Directory(props) {
         const directory = props.campsites.map(campsite => {
             return(
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+                    <RenderDirectoryItem campsite={campsite} />
                 </div>
             );
         }
