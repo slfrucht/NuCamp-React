6be770a8615c8547f89130232121be39f776e1ea
slfrucht <sfrucht@lmi.net>
2021-04-23 21:07:19 -0700

    add error testing and validation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import {BrowserRouter} from "react-router-dom";
 import Main from "./components/MainComponent";
 import {Provider} from "react-redux";
-import {ConfigureStore} from "./redux";
+import {ConfigureStore} from "./redux/configureStore";
 import './App.css';
 
 const store = ConfigureStore();
